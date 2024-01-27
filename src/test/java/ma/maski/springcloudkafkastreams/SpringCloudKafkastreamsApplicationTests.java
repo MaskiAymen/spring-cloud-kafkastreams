2a9583ea9608f1d00e4100b96af9c2d45994037f
msi <aymenmaski33@gmail.com>
2024-01-27 03:44:11 +0100

    commit

---
+++ src/test/java/ma/maski/springcloudkafkastreams/SpringCloudKafkastreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.maski.springcloudkafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudKafkastreamsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
